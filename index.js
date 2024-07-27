6961f150f9355e6d3fa649fe0f686e03c74d57f7
Reyaj Alam <reyajalam0425@gmail.com>
2024-07-27 16:28:24 +0530

    first oush in github

---
+++ index.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 5000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/twitter', (req, res) => {
+  res.send('feelreyaj')
+})
+app.get('/login', (req, res)=>{
+    res.send('<h1>This is my website</h1>')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
